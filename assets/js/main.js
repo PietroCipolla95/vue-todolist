7deb9ffbc497b254975579f9dd88773309d180a6
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-13 16:22:23 +0200

    add line-through to done = true

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
+/* 
+
+MILESTONE 1 Stampare all’interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+MILESTONE 2 Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus: 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+*/
+
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
+        
         tasks : [
             {
                 text : 'Pelare le patate 🥔',
@@ -28,7 +38,8 @@ const { createApp } = Vue
                 text : 'Portare auto dal meccanico 🔧',
                 done : false,
             },
-        ]
+        ],
+
       }
     }
   }).mount('#app')
