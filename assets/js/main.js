2d6c1283b61ccb821c385dbefd459c06395c59d4
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-09-13 14:57:19 +0200

    add tasks

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,32 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        tasks : [
+            {
+                text : 'Pelare le patate 🥔',
+                done : false,
+            },
+            {
+                text : 'Comprare regalo fidanzata 🎁',
+                done : true,
+            },
+            {
+                text : 'Cena con i colleghi 🥘',
+                done : false,
+            },
+            {
+                text : 'Serata Magic 🎴',
+                done : true,
+            },
+            {
+                text : 'Fare la spesa 🛒',
+                done : false,
+            },
+            {
+                text : 'Portare auto dal meccanico 🔧',
+                done : false,
+            },
+        ]
       }
     }
   }).mount('#app')
